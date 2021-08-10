f5535ab56a796d8f3bd4d93753f43b86d468f44c
20210112 <20210112@BCI20210112DS01.I-BCCARD.BIZ>
2021-08-10 17:16:24 +0900

    creating controller for the front html file.. what to do here? not decided yet. wanted to use RestTemplate, originally...

---
+++ src/main/java/com/rest/demo/FrontController.java
@@ -0,0 +1,7 @@
+package com.rest.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+}
